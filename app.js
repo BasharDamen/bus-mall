cbf4c37b4f193eecf8e644ab1633d453dca3ea1c
BasharDamen <bashar.alda@gmail.com>
2021-07-05 00:55:47 +0300

    changes

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 'use stirct'
-
-
+// const leftImageElement = document.getElementById('left-image');
+// const middleImageElement = document.getElementById('middle-image');
+// const rightImageElement = document.getElementById('right-image');
+// const images = document.getElementById('products')
 const images = document.getElementById('products');
-
-
 const maxAttempts = 25;
-
 let counter = 0;
-
 function BusMall(name, source) {
   this.name = name;
   this.source = source;
@@ -15,10 +13,7 @@ function BusMall(name, source) {
   this.votes = 0;
   BusMall.gloArr.push(this);
 }
-
-
 BusMall.gloArr = [];
-
 new BusMall('bag', 'img/bag.jpg');
 new BusMall('banana', 'img/banana.jpg');
 new BusMall('bathroom', 'img/bathroom.jpg');
@@ -39,49 +34,41 @@ new BusMall('unicorn', 'img/unicorn.jpg');
 new BusMall('water-can', 'img/water-can.jpg');
 new BusMall('wine-glass', 'img/wine-glass.jpg');
 console.log(BusMall.gloArr);
-
-
-
 function randomIndex() {
   return Math.floor(Math.random() * BusMall.gloArr.length);
 }
 
 
-
 let img1 = document.getElementById('Prod1')
 let img2 = document.getElementById('Prod2')
 let img3 = document.getElementById('Prod3')
+
+
 let leftIndex; 
 let middleIndex ;
 let rightIndex ; 
 
 
 function renderimgs() {
-  
     leftIndex = randomIndex();
   middleIndex = randomIndex();
   rightIndex = randomIndex();
   console.log(leftIndex);
   console.log(middleIndex);
   console.log(rightIndex);
-
     while (leftIndex === middleIndex
         || leftIndex === rightIndex
         || rightIndex === middleIndex) {
-
             leftIndex = randomIndex();
             middleIndex = randomIndex();
             rightIndex = randomIndex();
     }
-    
     img1.src = BusMall.gloArr[leftIndex].source;
     img2.src = BusMall.gloArr[middleIndex].source;
     img3.src = BusMall.gloArr[rightIndex].source;
-
     BusMall.gloArr[leftIndex].views ++;
     BusMall.gloArr[middleIndex].views ++;
     BusMall.gloArr[rightIndex].views ++;
-
 }
 renderimgs();
 
@@ -90,11 +77,25 @@ img1.addEventListener('click', handleClick)
 img2.addEventListener('click', handleClick)
 img3.addEventListener('click', handleClick)
 
+function renderList() {
+    const ul = document.getElementById('unlist');
+    
+    for (let i = 0; i < BusMall.gloArr.length; i++) {
+        let li = document.createElement('li');
+        ul.appendChild(li);
+        li.textContent = `${BusMall.gloArr[i].name} has this number of votes ( ${BusMall.gloArr[i].votes} ), and has showed up ( ${BusMall.gloArr[i].views} )`;
+        
+
+    }
+    img1.addEventListener('click', handleClick)
+    img2.addEventListener('click', handleClick)
+    img3.addEventListener('click', handleClick)
+}
+
 
 function handleClick(event) {
     counter++;
     console.log(event);
-
     if(maxAttempts >= counter){
         if (event.target.id === 'Prod1') {
             BusMall.gloArr[leftIndex].votes++;
@@ -104,27 +105,10 @@ function handleClick(event) {
             BusMall.gloArr[rightIndex].votes++;
         }
         renderimgs();
-    }else{
+    }
+    else{
         renderList();
     }
-    
 }
-
-function renderList() {
-    const ul = document.getElementById('unlist');
-    for (let i = 0; i < BusMall.gloArr.length; i++) {
-        let li = document.createElement('li');
-        ul.appendChild(li);
-        li.textContent = `${BusMall.gloArr[i].name} has this number of votes ( ${BusMall.gloArr[i].votes} ), and has showed up ( ${BusMall.gloArr[i].views} )`
-        
-    }
-    img1.addEventListener('click', handleClick)
-    img2.addEventListener('click', handleClick)
-    img3.addEventListener('click', handleClick)
-}
-
-
-
-
 
 
